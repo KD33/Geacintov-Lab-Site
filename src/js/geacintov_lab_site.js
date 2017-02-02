7668233efc53d78662e7737be1e042323584b19b
sammysamsam <sm4478@nyu.edu>
2017-02-02 16:39:46 -0500

    big styling changes and code organized much better and css split for each page

--- src/js/geacintov_lab_site.js
+++ src/js/geacintov_lab_site.js
@@ -1,34 +1,66 @@
 
+  var navbarfunction = function()
+  {
+      var header = $('#homepageheadercontainer'); 
+      if (header.width() >= 900 ) { 
+        $('#menu').removeClass().addClass('fullmenucontainer');
+        $('#item').removeClass().addClass('fullmenuitem');        
+        $('#item2').removeClass().addClass('fullmenuitem ');    
+        $('#itemselected').removeClass().addClass('fullmenuitemselected'); 
+        $('.dropdownelementburger').hide(); 
+        $('.filler').show();
+      }
+      else {
+        $('#menu').removeClass().addClass('menucontainer');
+        $('#item').removeClass().addClass('dropdownelement');        
+        $('#item2').removeClass().addClass('dropdownelement');     
+        $('#itemselected').removeClass().addClass('dropdownelementselected');       
+        $('.dropdownelementburger').show(); 
+        $('.filler').hide();
+      } 
+
+  }
+
+  var headerfunction = function()
+  {
+      var header = $('#homepageheadercontainer'); //this = window
+
+      if (header.width() < 1000) {
+        $('#X').css({"width":"60%"});
+        header.css({"background-size":"1000px 100%"});
+        $('#headertitle').css({"font-size":"40px"});
+        $('#description').css({"font-size":"14px"});
+       $('.overviewmain1').css({"font-size":"15px"});
+
+      }else{
+        $('#X').css({"width":"50%"});
+        header.css({"background-size":"100% 100%"});
+        $('#headertitle').css({"font-size":"52px"});
+        $('#description').css({"font-size":"16px"}); 
+        $('.overviewmain1').css({"font-size":"18px"}); 
+      }
+  }
+
+
 $( document ).ready(function() {
+    
+    navbarfunction();
+  	headerfunction();
 
-  	$('#dropdownelementburger').click(function() {
-
+    $('.dropdownelementburger').click(function() {
   		  if ($('.dropdownelement').is(":visible")) {
-
   			  	$('.dropdownelement').hide();
   			  	$('.dropdownelementselected').hide();
-  			  	$('#sidebar').css({"height":"50px","transition":"height 500ms ease-in-out"});
-
-  		  } else {
-
-  			    $('#sidebar').css({"height":"270px","transition":"height 500ms ease-in-out"});
+  			  	$('.menucontainer').css({"height":"50px","transition":"height 500ms ease-in-out"});
+        } else {
             $('.dropdownelementselected').show(500);
-            $('.dropdownelement').show(500);   
-
-  	  		}
+           $('.dropdownelement').show(500);   
+            $('.menucontainer').css({"height":"270px","transition":"height 500ms ease-in-out"});
+          }
   	});
 
   	$('.carousel').carousel('pause');
-
     $('[data-toggle="tooltip"]').tooltip();   
-
-    var header = $('#homepageheadercontainer'); //this = window
-
-    if (header.width() < 1100) {
-    	header.css({"background-size":"1099px 100%"});
-    }else{
-    	header.css({"background-size":"100% 100%"});
-    }
 });
 
 
@@ -38,14 +70,8 @@ $(document).on('mouseleave', '.carousel', function() {
 
 
 $(window).on('resize', function(){
-      var header = $('#homepageheadercontainer'); //this = window
-
-      if (header.width() < 1100) {
-      	header.css({"background-size":"1099px 100%"});
-      }else{
-      	header.css({"background-size":"100% 100%"});
-      }
-
+    navbarfunction();
+    headerfunction();
 });
 
 
