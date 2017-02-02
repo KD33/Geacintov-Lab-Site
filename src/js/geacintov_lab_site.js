ba30b9076f9a11068d3744d9a1b88c8e03dd8c68
sammysamsam <sm4478@nyu.edu>
2017-02-01 22:59:34 -0500

    updated styles and navigation bar for homepage, updated html for people (card/profile style)

--- src/js/geacintov_lab_site.js
+++ src/js/geacintov_lab_site.js
@@ -1,40 +1,51 @@
 
 $( document ).ready(function() {
-	$('#dropdownelementburger').click(function() {
-		  if ($('.dropdownelement').is(":visible")) {
-			  	$('.dropdownelement').hide();
-			  	$('.dropdownelementselected').hide();
-			  	$('#sidebar').css({"height":"50px","transition":"height 500ms ease-in-out"});
-		  } else {
-			    $('#sidebar').css({"height":"270px","transition":"height 500ms ease-in-out"});
+
+  	$('#dropdownelementburger').click(function() {
+
+  		  if ($('.dropdownelement').is(":visible")) {
+
+  			  	$('.dropdownelement').hide();
+  			  	$('.dropdownelementselected').hide();
+  			  	$('#sidebar').css({"height":"50px","transition":"height 500ms ease-in-out"});
+
+  		  } else {
+
+  			    $('#sidebar').css({"height":"270px","transition":"height 500ms ease-in-out"});
             $('.dropdownelementselected').show(500);
-             $('.dropdownelement').show(500);   
-	  		}
-	});
+            $('.dropdownelement').show(500);   
 
-	$('.carousel').carousel('pause');
+  	  		}
+  	});
 
-   	$('[data-toggle="tooltip"]').tooltip();   
+  	$('.carousel').carousel('pause');
 
-      var header = $('#homepageheadercontainer'); //this = window
-      if (header.width() < 1100) {
-      	header.css({"background-size":"1099px 100%"});
-      }else{
-      	header.css({"background-size":"100% 100%"});
-      }
+    $('[data-toggle="tooltip"]').tooltip();   
 
+    var header = $('#homepageheadercontainer'); //this = window
 
+    if (header.width() < 1100) {
+    	header.css({"background-size":"1099px 100%"});
+    }else{
+    	header.css({"background-size":"100% 100%"});
+    }
 });
+
+
 $(document).on('mouseleave', '.carousel', function() {
     $('.carousel').carousel('pause');
 });
+
+
 $(window).on('resize', function(){
       var header = $('#homepageheadercontainer'); //this = window
+
       if (header.width() < 1100) {
       	header.css({"background-size":"1099px 100%"});
       }else{
       	header.css({"background-size":"100% 100%"});
       }
+
 });
 
 
